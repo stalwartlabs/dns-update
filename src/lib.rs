0dc43f2ace9914e3af2a01897594777fcf688d5f
Maurus Decimus <11444311+mdecimus@users.noreply.github.com>
2026-04-02 10:29:37 +0200

    Derive Eq, Hash

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@ pub enum Error {
 }
 
 /// A DNS record type.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum DnsRecordType {
     A,
     AAAA,
@@ -60,14 +60,14 @@ pub enum DnsRecordType {
 }
 
 /// A named DNS record, which consists of a name and a DNS record.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct NamedDnsRecord {
     pub name: String,
     pub record: DnsRecord,
 }
 
 /// A DNS record type with a value.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum DnsRecord {
     A(Ipv4Addr),
     AAAA(Ipv6Addr),
@@ -81,7 +81,7 @@ pub enum DnsRecord {
 }
 
 // An MX record, which consists of an exchange string and a priority.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 
 pub struct MXRecord {
     pub exchange: String,
@@ -89,7 +89,7 @@ pub struct MXRecord {
 }
 
 // A SRV record, which consists of a target string, priority, weight, and port.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct SRVRecord {
     pub target: String,
     pub priority: u16,
@@ -98,7 +98,7 @@ pub struct SRVRecord {
 }
 
 // A TLSA record, which consists of a certificate usage, selector, matching type, and certificate data.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct TLSARecord {
     pub cert_usage: TlsaCertUsage,
     pub selector: TlsaSelector,
@@ -131,7 +131,7 @@ pub enum TlsaMatching {
 }
 
 // A CAA record, which can be either an Issue, IssueWild, or Iodef record.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum CAARecord {
     Issue {
         issuer_critical: bool,
@@ -149,7 +149,7 @@ pub enum CAARecord {
     },
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct KeyValue {
     pub key: String,
     pub value: String,
